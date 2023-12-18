0e1f14e25d9ff23f24b6052eef3269b71a9f1809
chiara1911 <chiaracesari89@gmail.com>
2023-12-18 14:50:51 +0100

    add image comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,16 @@
 @section('content')
 <main>
     <h1>Home page</h1>
+    <div class="container">
+        <h2>DC COMICS</h2>
+        <div class="row">
+            @foreach ($comics as $comic)
+            <div class="col-12 col-md-4 col-lg-3 mt-2">
+                <img src="{{$comic['thumb']}}" alt="">
+            </div>
+
+            @endforeach
+        </div>
+    </div>
 </main>
 @endsection
