1d67256020d7a59805562d94fde065af2e785caa
chiara1911 <chiaracesari89@gmail.com>
2023-12-19 15:15:24 +0100

    add array in another db, fix layout

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,22 +3,18 @@
 @section('title', 'Movies')
 
 @section('content')
-
-
     <main>
-
         <div class="container">
-
             <div class="row">
                 <div class="col-12 m-4">
                     <div class="d-flex flex-row">
                         <img src="{{ $movie['thumb'] }}" alt="{{ $movie['series'] }}" class="big-image">
-                        <div class="d-flex flex-column px-4 text-light text-uppercase">
-                            <h3> title :{{ $movie['title'] }}</h3>
-                            <h4>description {{ $movie['description'] }}</h4>
+                        <div class="d-flex flex-column px-4 text-uppercase opacity-75 ">
+                            <h3 class="text-light"> title :{{ $movie['title'] }}</h3>
+                            <h4 class="text-light">description : {{ $movie['description'] }}</h4>
                             <div class="mt-3 d-flex justify-content-between ">
                                 <span class="btn btn-primary  p-3">{{ $movie['price'] }}</span>
-                                <div class="d-flex flex-column">
+                                <div class="d-flex flex-column text-light">
                                     <span class="d-flex flex-column">data di uscita :</span>
                                         <span>{{ $movie['sale_date'] }}</span>
                                 </div>
